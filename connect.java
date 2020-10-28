a3660d867389dcd4a0df103bff20a156797e95e1
vinutha2505 <73499492+vinutha2505@users.noreply.github.com>
2020-10-28 15:15:40 -0500

    Update connect.java

--- connect.java
+++ connect.java
@@ -3,7 +3,7 @@ package firstworld;
 public class connect {
 	public static void main (String [] args){
 		
-		System.out.println("hello world 2020 november");
+		System.out.println("hello world 2020 november first");
 
 	}
 	}
